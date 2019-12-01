20a86b05ba4ae404915d4f23681c99771e50d331
vagrant <vagrant@localhost.localdomain>
2019-12-01 08:40:06 +0000

    Initial Release

---
+++ views/default/page/elements/header_logo.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Elgg header logo
+ */
+
+$site = elgg_get_site_entity();
+$site_name = $site->name;
+$site_url = elgg_get_site_url();
+?>
+
+<h1>
+   
+    <?php
+    
+    ?>
+	<a class="elgg-heading-site" href="<?php //echo $site_url; ?>">
+		<?php //echo $site_name; ?>
+	</a>
+</h1>
