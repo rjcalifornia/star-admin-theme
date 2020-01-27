e9fb1f96b7af3e04aa33b014ccafd3ee305f0395
vagrant <vagrant@vagrant.vm>
2020-01-27 21:06:38 +0000

    More changes to the profile

--- views/default/custom_index/star_landing_home.php
+++ views/default/custom_index/star_landing_home.php
@@ -10,55 +10,15 @@ $site = elgg_get_site_entity();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Simple landing page</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="<?php echo $site_url?>mod/star-admin-theme/vendors/landing_assets/owl-carousel/css/owl.carousel.min.css">
-  <link rel="stylesheet" href="<?php echo $site_url?>mod/star-admin-theme/vendors/landing_assets/owl-carousel/css/owl.theme.default.css">
-  <link rel="stylesheet" href="<?php echo $site_url?>mod/star-admin-theme/vendors/landing_assets/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="<?php echo $site_url?>mod/star-admin-theme/vendors/landing_assets/aos/css/aos.css">
-  <link rel="stylesheet" href="<?php echo $site_url?>mod/star-admin-theme/vendors/landing_assets/css/style.min.css">
+<?php 
+echo elgg_view('custom_index/elements/star_landing_head');
+?>
 </head>
 <body id="body" data-spy="scroll" data-target=".navbar" data-offset="100">
   <header id="header-section">
-    <nav class="navbar navbar-expand-lg pl-3 pl-sm-0" id="navbar">
-    <div class="container">
-      <div class="navbar-brand-wrapper d-flex w-100">
-        <img src="<?php echo $site_url?>mod/star-admin-theme/vendors/landing_assets/images/Group2.svg" alt="">
-        <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="mdi mdi-menu navbar-toggler-icon"></span>
-        </button> 
-      </div>
-      <div class="collapse navbar-collapse navbar-menu-wrapper" id="navbarSupportedContent">
-        <ul class="navbar-nav align-items-lg-center align-items-start ml-auto">
-          <li class="d-flex align-items-center justify-content-between pl-4 pl-lg-0">
-            <div class="navbar-collapse-logo">
-              <img src="<?php echo $site_url?>mod/star-admin-theme/vendors/landing_assets/images/Group2.svg" alt="">
-            </div>
-            <button class="navbar-toggler close-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="mdi mdi-close navbar-toggler-icon pl-5"></span>
-            </button>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#header-section">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#features-section">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#digital-marketing-section">Blog</a>  
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#feedback-section">Testimonials</a>
-          </li>
-          <li class="nav-item btn-contact-us pl-4 pl-lg-0">
-            <button class="btn btn-info" data-toggle="modal" data-target="#exampleModal">Contact Us</button>
-          </li>
-        </ul>
-      </div>
-    </div> 
-    </nav>   
+<?php 
+    echo elgg_view('custom_index/elements/star_landing_nav');
+?>
   </header>
   <div class="banner" >
     <div class="container">
@@ -73,39 +33,10 @@ $site = elgg_get_site_entity();
   </div>
   <div class="content-wrapper">
     <div class="container">
-      <section class="features-overview" id="features-section" >
-        <div class="content-header">
-          <h2>How does it works</h2>
-          <h6 class="section-subtitle text-muted">One theme that serves as an easy-to-use operational toolkit<br>that meets customer's needs.</h6>
-        </div>
-        <div class="d-md-flex justify-content-between">
-          <div class="grid-margin d-flex justify-content-start">
-            <div class="features-width">
-              <img src="images/Group12.svg" alt="" class="img-icons">
-              <h5 class="py-3">Speed<br>Optimisation</h5>
-              <p class="text-muted">Lorem ipsum dolor sit amet, tincidunt vestibulum. Fusce egeabus consectetuer turpis, suspendisse.</p>
-              <a href="#"><p class="readmore-link">Readmore</p></a>  
-            </div>
-          </div>
-          <div class="grid-margin d-flex justify-content-center">
-            <div class="features-width">
-              <img src="images/Group7.svg" alt="" class="img-icons">
-              <h5 class="py-3">SEO and<br>Backlinks</h5>
-              <p class="text-muted">Lorem ipsum dolor sit amet, tincidunt vestibulum. Fusce egeabus consectetuer turpis, suspendisse.</p>
-              <a href="#"><p class="readmore-link">Readmore</p></a>
-            </div>
-          </div>
-          <div class="grid-margin d-flex justify-content-end">
-            <div class="features-width">
-              <img src="images/Group5.svg" alt="" class="img-icons">
-              <h5 class="py-3">Content<br>Marketing</h5>
-              <p class="text-muted">Lorem ipsum dolor sit amet, tincidunt vestibulum. Fusce egeabus consectetuer turpis, suspendisse.</p>
-              <a href="#"><p class="readmore-link">Readmore</p></a>
-            </div>
-          </div>
-        </div>
-      </section>     
-      <section class="digital-marketing-service" id="digital-marketing-section">
+        <?php
+        echo elgg_view('custom_index/sections/landing_features');
+        ?>
+       <section class="digital-marketing-service" id="digital-marketing-section">
         <div class="row align-items-center">
           <div class="col-12 col-lg-7 grid-margin grid-margin-lg-0" data-aos="fade-right">
             <h3 class="m-0">We Offer a Full Range<br>of Digital Marketing Services!</h3>
@@ -383,7 +314,8 @@ $site = elgg_get_site_entity();
         </div>  
       </section>
       <footer class="border-top">
-        <p class="text-center text-muted pt-4">Copyright ¬© 2019<a href="https://www.bootstrapdash.com/" class="px-1">Bootstrapdash.</a>All rights reserved.</p>
+        <p class="text-center text-muted pt-4">About | Terms | Privacy | Domain policy</p>
+        
       </footer>
       <!-- Modal for Contact - us Button -->
       <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -417,10 +349,10 @@ $site = elgg_get_site_entity();
       </div>
     </div> 
   </div>
-  <script src="<?php echo $site_url?>mod/star-admin-theme/vendors/landing_assets/jquery/jquery.min.js"></script>
-  <script src="<?php echo $site_url?>mod/star-admin-theme/vendors/landing_assets/bootstrap/bootstrap.min.js"></script>
-  <script src="<?php echo $site_url?>mod/star-admin-theme/vendors/landing_assets/owl-carousel/js/owl.carousel.min.js"></script>
-  <script src="<?php echo $site_url?>mod/star-admin-theme/vendors/landing_assets/aos/js/aos.js"></script>
-  <script src="<?php echo $site_url?>mod/star-admin-theme/vendors/landing_assets/js/landingpage.js"></script>
+    
+    <?php
+    echo elgg_view('custom_index/elements/star_landing_foot');
+    ?>
+    
 </body>
 </html>
